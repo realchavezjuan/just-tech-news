2af7971e3cdc321322c54c6ee6ce71c9328561b5
Juan Chavez <juan@Juans-Air.home>
2022-02-08 02:08:04 -0800

    post routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./user');
-const Post = require('./post')
+const Post = require('./post');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});  
+
 module.exports = { User, Post };
