c08f404a4c13739d2a772c9ed4a2ee23571dbd39
Juan Chavez <juan@Juans-Air.home>
2022-02-10 18:57:05 -0800

    handlebars setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 // gets ./api/index.js
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 // adds /api to the api route
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
